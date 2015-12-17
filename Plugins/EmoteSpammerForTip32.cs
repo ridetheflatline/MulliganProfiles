385f5079f4f79dbd031eab1881dd486cc1aec11b
Arthur Semjonov <arturs.semjonovs@mu.edu>
2015-12-16 18:18:23 -0600

    lets see

---
+++ Plugins/EmoteSpammerForTip32.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SmartBot.Plugins;
+using SmartBot.Plugins.API;
+
+
+namespace RopeToVictory
+{
+    [Serializable]
+    public class EmoteSpammerForTip32 : PluginDataContainer
+    {
+        //Init vars
+        public EmoteSpammerForTip32()
+        {
+            Name = "RopeToVictory";
+        }
+
+    }
+
+    public class EmoteSpammerForTip32 : Plugin
+    {
+        public override async void OnTurnBegin()
+        {
+            Bot.SuspendBot();
+            await Task.Delay(20000);
+            Bot.ResumeBot();
+        }
+        public override void OnTick()
+        {
+            Bot.SendEmote(1);
+        }
+
+    }
+}
