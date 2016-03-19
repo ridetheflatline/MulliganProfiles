d4c192e2c01c8ee5f4fc9bbb3ad48f4e634f6e70
ArthurFairchild <supervalid@mymail.com>
2016-03-18 20:50:16 -0500

    masterwai example

--- MulliganProfiles/SmartMulliganV3.cs
+++ MulliganProfiles/SmartMulliganV3.cs
@@ -12,6 +12,7 @@ using SmartBot.Plugins.API;
 
 namespace MulliganProfiles
 {
+    #region Extension class
     public static class Extension
     {
         public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> map, TKey key, TValue value)
@@ -19,14 +20,38 @@ namespace MulliganProfiles
             map[key] = value;
         }
 
+        public static void AddAll<TKey, TValue>(this IDictionary<TKey, TValue> map, TValue value, params TKey[] keys)
+        {
+            foreach(TKey key in keys)
+                map.AddOrUpdate(key, value);
+        }
+
+        public static void BlackListAll<TKey, TValue>(this IDictionary<TKey, TValue> map, params TKey[] keys)
+        {
+            foreach (TKey key in keys.Where(map.ContainsKey))
+                map.Remove(key);
+        }
+
+        public static bool IsMinion(this Card.Cards card)
+        {
+            return CardTemplate.LoadFromId(card).Type == Card.CType.MINION;
+        }
+
+        public static bool IsSpell(this Card.Cards card)
+        {
+            return CardTemplate.LoadFromId(card).Type == Card.CType.SPELL;
+        }
+
+        public static bool IsWeapon(this Card.Cards card)
+        {
+            return CardTemplate.LoadFromId(card).Type == Card.CType.MINION;
+        }
         public static int TurnPlayCount(this IList<Card.Cards> deck, int turn)
         {
             return deck.Count(cards => CardTemplate.LoadFromId(cards).Cost == turn);
         }
         /// <summary>
-        /// Item1 = number of minions
-        /// Item2 = number of spells
-        /// Item3 = number of weapons
+        /// This is a useless method
         /// </summary>
         /// <param name="deck"></param>
         /// <returns></returns>
@@ -39,46 +64,82 @@ namespace MulliganProfiles
                 deck.Count(cards => CardTemplate.LoadFromId(cards).Type == Card.CType.WEAPON)
             );
         }
-        public static Tuple<List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>> SplitByTurnList(this IList<Card.Cards> list)
+        /// <summary>
+        /// this is a very useless method that I realized was useless only after I used it... once, like ever... It's sad, please don't code like this
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static Tuple<List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>> SplitByTurnList(this IList<Card.Cards> list)
         {
-            return new Tuple<List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>>
+            return new Tuple<List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>, List<Card.Cards>>
                 (
-                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 0).ToList(),
-                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 1).ToList(),
-                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 2).ToList(),
-                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 3).ToList(),
-                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 4).ToList()
+                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 0).ToList(), //all 0 drops
+                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 1).ToList(), //all 1 drops
+                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 2).ToList(), //all 2 drops
+                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 3).ToList(), //all 3 drops
+                    list.Where(card => CardTemplate.LoadFromId(card).Cost == 4).ToList(), //all 4 drops
+                    list.Where(card => CardTemplate.LoadFromId(card).Cost > 4).ToList()   //all 5+ drops
                 );
         }
 
     }
-    
+    #endregion
+
     public class GameContainer 
 
     {
 
         public DeckType MyDeckType { get; set; }
-        public Style MyStyle;
-        public List<Card.Cards> MyDeck;
-        public DeckType EneDeckType;
-        public Style EnemyStyle;
-        public List<Card.Cards> EnemyDeck;
-        public Dictionary<Dictionary<DeckType, Style>, List<Card.Cards>> EnemyHistory;
-        public long UniqueUlong;
+        public Style MyStyle { get; set; }
+        public List<Card.Cards> MyDeck { get; set; }
+        public DeckType EneDeckType { get; set; }
+        public Style EnemyStyle { get; set; }
+        
+        private const string Mode = "Mode";
+        private const string TrackerMyType = "AutoFriendlyDeckType";
+        private const string TrackerForceMyType = "ForcedDeckType";
+        private const string TrackerMyStyle = "AutoFriendlyStyle";
+        private const string TrackerEnemyType = "EnemyDeckTypeGuess";
+        private const string TrackerEnemyStyle = "EnemyDeckStyleGuess";
+        private const string MulliganTesterMyDeck = "MulliganTesterYourDeck";
+        private const string MulliganTesterEnemyDeck = "MulliganTEsterEnemyDeck";
+        private const string EnumsCount = "SynchEnums";
+        public List<Card.Cards> Choices { get; set; }
+        public Card.CClass OpponentClass { get; set; }
+        public Card.CClass OwnClass { get; set; }
 
-        private const string TrackerMyType = "AutoRecognizeDeckType";
-        private const string TrackerMyStyle = "";
-        private const string TrackerEnemyType = "";
-        private const string TrackerEnemyStyle = "";
-        public List<Card.Cards> Choices;
-        public Card.CClass OpponentClass;
-        public Card.CClass OwnClass;
+        /// <summary>
+        /// All of the below drops originate from choices
+        /// </summary>
+
+        public List<Card.Cards> ZeroDrops { get; set; }
+        public List<Card.Cards> OneDrops { get; set; }
+        public List<Card.Cards> TwoDrops { get; set; }
+        public List<Card.Cards> ThreeDrops { get; set; }
+        public List<Card.Cards> FourDrops { get; set; }
+        public List<Card.Cards> FivePlusDrops { get; set; }
+
+        public bool HasTurnOne { get; set; }
+        public bool HasTurnTwo { get; set; }
+        public bool HasTurnThree { get; set; }
+        
+        public bool Coin { get; set; }
+        public Tuple<List<Card.Cards>,
+            List<Card.Cards>, List<Card.Cards>,
+            List<Card.Cards>, List<Card.Cards>,
+            List<Card.Cards>> AllDropsTuple { get; set; }
+
+
+
 
         public GameContainer(List<Card.Cards> choices, Card.CClass opponentClass, Card.CClass ownClass)
         {
-            this.Choices = choices;
-            this.OpponentClass = opponentClass;
-            this.OwnClass = ownClass;
+            
+            Choices = choices;
+            OpponentClass = opponentClass;
+            OwnClass = ownClass;
+            Coin = choices.Count > 3;
+            #region SmartTracker
             Plugin tracker = Bot.GetPlugins().Find(plugin => plugin.DataContainer.Name == "SmartTracker");
 
             if (!tracker.DataContainer.Enabled)
@@ -87,24 +148,59 @@ namespace MulliganProfiles
                 Bot.StopBot();
             }
             Dictionary<string, object> properties = tracker.GetProperties();
-            if (Enum.GetNames(typeof (DeckType)).Length != (int) properties["synchEnums"])
+            //PrintDebug(properties);
+            if (Enum.GetNames(typeof (DeckType)).Length != (int) properties[EnumsCount])
             {
                 Bot.Log("[URGENT!!!!] Arthur, your enums are out of synch");
             }
-            MyDeckType = properties["mode"].ToString() == "Manual" 
-                    ? (DeckType) properties["ForceDeckType"] //if Manual
+            MyDeckType = properties[Mode].ToString() == "Manual" 
+                    ? (DeckType) properties[TrackerForceMyType] //if Manual
                     : (DeckType) properties[TrackerMyType];  //if Auto
             
-            if (properties["mode"].ToString() == "Manual")
+            if (properties[Mode].ToString() == "Manual")
                 Bot.Log("[SmartMulliganV3] Dear friends, I notice that you are forcing deck recognition." +
                         " I do not make failsafe checks on whether or not you are using a proper deck, " +
                         "so if you decide to force Camel Hunter while playing FaceFreeze mage. It will let you. I hope you know what you are doing.");
-                Bot.Log(string.Format("[You chose {0} Detection] {1} [Default: AutoDetection] {2}", properties["mode"], MyDeckType, properties["AutoRecognizeDeckType"]));
-
+                Bot.Log(string.Format("[You chose {0} Detection] {1} [Default: AutoDetection] {2}", properties[Mode], MyDeckType, properties[TrackerMyType]));
+            #endregion 
             MyStyle = (Style) properties[TrackerMyStyle];
             EneDeckType = (DeckType) properties[TrackerEnemyType];
             EnemyStyle = (Style) properties[TrackerEnemyStyle];
+            AllDropsTuple = choices.SplitByTurnList();
+            ZeroDrops = AllDropsTuple.Item1;
+            OneDrops = AllDropsTuple.Item2;
+            TwoDrops = AllDropsTuple.Item3;
+            ThreeDrops = AllDropsTuple.Item4;
+            FourDrops = AllDropsTuple.Item5;
+            FivePlusDrops = AllDropsTuple.Item6;
+            HasTurnOne = false;
+            HasTurnTwo = false;
+            HasTurnThree = false;
+
         }
+
+        private void PrintDebug(Dictionary<string, object> properties)
+        {
+        Bot.Log(string.Format("==============" +
+                              "\nMy Type: {0}" +
+                              "\nMy Forced Type: {1}" +
+                              "\nMy Style: {2}" +
+                              "\nEnemy Type: {3}" +
+                              "\nEnemy Style: {4}" +
+                              "\nMulligan Tester <me>: {5}" +
+                              "\nMulligan Tester <him>: {6}" +
+                              "\nEnum Count: {7}" +
+                              "\t", properties[TrackerMyType], 
+                              properties[TrackerForceMyType], 
+                              properties[TrackerMyStyle], 
+                              properties[TrackerEnemyType], 
+                              properties[TrackerEnemyStyle],
+                              properties[MulliganTesterMyDeck],
+                              properties[MulliganTesterEnemyDeck],
+                              properties[EnumsCount]) 
+                              );
+        }
+
 
         public GameContainer()
         {
@@ -130,9 +226,9 @@ namespace MulliganProfiles
 
             _whiteList = new Dictionary<Card.Cards, bool>
             {
-                { Card.Cards.GAME_005, true },//coin
-                { Cards.Innervate, true },
-                { Cards.WildGrowth, false }
+                { Card.Cards.GAME_005, true },  //coin
+                { Cards.Innervate, true },      //always keeps 2 innervates (set false for 1)
+                { Cards.WildGrowth, false }     //only keeps 1 wild growth
             };
 
             _cardsToKeep = new List<Card.Cards>();
@@ -141,9 +237,9 @@ namespace MulliganProfiles
 
         public List<Card.Cards> HandleMulligan(List<Card.Cards> choices, Card.CClass opponentClass, Card.CClass ownClass)
         {
-
-            GameContainer newGame = new GameContainer(choices, opponentClass, ownClass);
-            switch(newGame.MyDeckType)
+           GameContainer gameContainer = new GameContainer(choices, opponentClass, ownClass);
+            
+            switch (gameContainer.MyDeckType)
             {
                 case DeckType.Unknown:
                     break;
@@ -163,8 +259,12 @@ namespace MulliganProfiles
                     break;
                 case DeckType.FaceWarrior:
                     break;
+                case DeckType.RenoWarrior:
+                    break;
+                case DeckType.TauntWarrior:
+                    break;
                 case DeckType.SecretPaladin:
-                    HandleSecretPaladin(newGame);
+                    HandleSecretPaladin(gameContainer);
                     break;
                 case DeckType.MidRangePaladin:
                     break;
@@ -173,6 +273,8 @@ namespace MulliganProfiles
                 case DeckType.AggroPaladin:
                     break;
                 case DeckType.AnyfinMurglMurgl:
+                    break;
+                case DeckType.RenoPaladin:
                     break;
                 case DeckType.RampDruid:
                     break;
@@ -189,6 +291,12 @@ namespace MulliganProfiles
                 case DeckType.MechDruid:
                     break;
                 case DeckType.AstralDruid:
+                    break;
+                case DeckType.MillDruid:
+                    break;
+                case DeckType.BeastDruid:
+                    break;
+                case DeckType.RenoDruid:
                     break;
                 case DeckType.Handlock:
                     break;
@@ -208,6 +316,8 @@ namespace MulliganProfiles
                     break;
                 case DeckType.MalyLock:
                     break;
+                case DeckType.ControlWarlock:
+                    break;
                 case DeckType.TempoMage:
                     break;
                 case DeckType.FreezeMage:
@@ -221,6 +331,8 @@ namespace MulliganProfiles
                 case DeckType.EchoMage:
                     break;
                 case DeckType.FatigueMage:
+                    break;
+                case DeckType.RenoMage:
                     break;
                 case DeckType.DragonPriest:
                     break;
@@ -242,6 +354,10 @@ namespace MulliganProfiles
                     break;
                 case DeckType.CamelHunter:
                     break;
+                case DeckType.RenoHunter:
+                    break;
+                case DeckType.DragonHunter:
+                    break;
                 case DeckType.OilRogue:
                     break;
                 case DeckType.PirateRogue:
@@ -255,6 +371,12 @@ namespace MulliganProfiles
                 case DeckType.FatigueRogue:
                     break;
                 case DeckType.MiracleRogue:
+                    break;
+                case DeckType.MechRogue:
+                    break;
+                case DeckType.RenoRogue:
+                    break;
+                case DeckType.MillRogue:
                     break;
                 case DeckType.FaceShaman:
                     break;
@@ -272,12 +394,15 @@ namespace MulliganProfiles
                     break;
                 case DeckType.BattleryShaman:
                     break;
+                case DeckType.RenoShaman:
+                    break;
                 case DeckType.Basic:
                     break;
-               
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
 
-
+            
             //HandleMinions(choices, _whiteList, myInfo);
             foreach (var s in from s in choices let keptOneAlready = _cardsToKeep.Any(c => c.ToString() == s.ToString()) where _whiteList.ContainsKey(s) where !keptOneAlready | _whiteList[s] select s)
                 _cardsToKeep.Add(s);
@@ -285,15 +410,29 @@ namespace MulliganProfiles
             return _cardsToKeep;
         }
 
-        private void HandleSecretPaladin(GameContainer newGame)
+        private void HandleSecretPaladin(GameContainer gameContainer)
         {
-            //1 = minion || 2 = spells || 3 = weapons
-            var information = newGame.Choices.NumOfTypes();
-            switch (newGame.OpponentClass)
+            Bot.Log("[Kappa] Entered Secret Paladin");
+            foreach (var q in gameContainer.OneDrops.Where(card => card.IsMinion()))
             {
-                
+                _whiteList.AddOrUpdate(q, false);
+                gameContainer.HasTurnOne = true;
             }
-            
+            foreach (var q in gameContainer.TwoDrops.Where(card => card.IsMinion()))
+            {
+                _whiteList.AddOrUpdate(q, false);
+                gameContainer.HasTurnTwo = true;
+            }
+            foreach (var q in gameContainer.ThreeDrops.Where(card => card.IsMinion()))
+            {
+                if (q.IsSpell())
+                    _whiteList.AddOrUpdate(q, false);
+                if (!gameContainer.HasTurnTwo) continue; //continue instead of break in case our allowed 3 drop spell is later in choices
+                _whiteList.AddOrUpdate(q, false);
+                gameContainer.HasTurnThree = true;
+            }
+            foreach (var q in gameContainer.FourDrops.Where(card => card.IsMinion()))
+                _whiteList.AddOrUpdate(q, false);
         }
     }
 
@@ -319,6 +458,7 @@ namespace MulliganProfiles
         DragonPaladin,
         AggroPaladin,
         AnyfinMurglMurgl,
+        RenoPaladin,
         /*Druid*/
         RampDruid,
         AggroDruid,
@@ -331,7 +471,6 @@ namespace MulliganProfiles
         MillDruid,
         BeastDruid,
         RenoDruid,
-        RenoPaladin,
         /*Warlock*/
         Handlock,
         RenoLock,
@@ -351,6 +490,7 @@ namespace MulliganProfiles
         MechMage,
         EchoMage,
         FatigueMage,
+        RenoMage,
         /*Priest*/
         DragonPriest,
         ControlPriest,
