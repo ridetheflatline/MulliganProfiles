c1ce7aa46eb893b264d5b0c9d38b4904e652ad6a
ArthurFairchild <supervalid@mymail.com>
2016-06-15 05:55:42 -0500

    More Misc

--- Plugins/Arthurs Bundle - Miscellaneous.cs
+++ Plugins/Arthurs Bundle - Miscellaneous.cs
@@ -11,15 +11,21 @@ namespace SmartBot.Plugins
     public class ArthursBundleMiscellaneous : PluginDataContainer
     {
 
-
+        [DisplayName("Stop when legend")]
         public bool sLegend { get; set; }
+        [DisplayName("Stop at rank X legend")]
         public int sLegendRank { get; set; }
-
+        [DisplayName("Auto-Concede\nUnwinable games")]
+        public bool AC { get; private set; } 
+        [DisplayName("Auto Concede:")]
+        public string ACDetails{ get; private set;}
         public ArthursBundleMiscellaneous()
         {
             Name = "Arthurs Bundle - Miscellaneous";
             sLegend = false;
             sLegendRank = 1000;
+            AC = false;
+            ACDetails = "It's more of a todo list than a working feature.\nDon't expect it to be updated anytime soon ";
 
         }
 
