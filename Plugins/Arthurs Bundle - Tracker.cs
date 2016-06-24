969422346d8aa54ae1906f9115c79b9955013eb2
ArthurFairchild <supervalid@mymail.com>
2016-06-23 21:59:31 -0500

    version

--- Plugins/Arthurs Bundle - Tracker.cs
+++ Plugins/Arthurs Bundle - Tracker.cs
@@ -168,7 +168,7 @@ namespace SmartBot.Plugins
             donation = "http://bit.ly/ABDonationLink";
             discord = "https://discord.gg/0wJubFLk1fKTb4Vx";
             vKey = GetLocalSha("Arthurs Bundle - Tracker.cs");
-            Versions = "4.013";
+            Versions = "4.014";
         }
         public void ReloadDictionary()
 
